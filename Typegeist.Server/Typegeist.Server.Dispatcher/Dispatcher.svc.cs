268ad88b51ac41bbf0d2c34e1476b8d5c3d2e6e8
Colin Bayer <vogon@icculus.org>
2011-06-05 21:36:51 -0700

    hello world dispatcher service

---
+++ Typegeist.Server/Typegeist.Server.Dispatcher/Dispatcher.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Diagnostics;
+
+namespace Typegeist.Server.Dispatcher
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class Dispatcher : IDispatcher
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public void SubmitResult(TypegeistResult result)
+        {
+            Debug.WriteLine("SubmitResult!");
+        }
+    }
+}
---
+++ Typegeist.Server/Typegeist.Server.Dispatcher/IDispatcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Typegeist.Server.Dispatcher
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IDispatcher
+    {
+        [OperationContract]
+        [WebInvoke(
+            Method = "POST",
+            BodyStyle = WebMessageBodyStyle.Bare
+        )]
+        void SubmitResult(TypegeistResult result);
+
+        [OperationContract]
+        [WebGet]
+        string GetData(int value);
+    }
+
+
+    [DataContract]
+    public class TypegeistResult
+    {
+        [DataMember]
+        public string Url { get; set; }
+
+        [DataMember]
+        public Dictionary<string, int> FontFamilyCount { get; set; }
+    }
+}

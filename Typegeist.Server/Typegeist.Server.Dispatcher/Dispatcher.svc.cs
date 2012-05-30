abead7b775e839148383ed13226966f33a0817a4
Colin Bayer <vogon@icculus.org>
2012-05-29 20:07:25 -0700

    some stuff I did ages ago and forget what it is?

--- Typegeist.Server/Typegeist.Server.Dispatcher/Dispatcher.svc.cs
+++ Typegeist.Server/Typegeist.Server.Dispatcher/Dispatcher.svc.cs
@@ -6,10 +6,13 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 using System.Diagnostics;
+using System.Data;
+using System.Data.Objects;
+
+using Typegeist.Server.Data;
 
 namespace Typegeist.Server.Dispatcher
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     public class Dispatcher : IDispatcher
     {
         public string GetData(int value)
---
+++ Typegeist.Server/Typegeist.Server.Dispatcher/DispatcherDataModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Objects;
+using System.Linq;
+using System.Web;
+using Typegeist.Server.Data;
+
+namespace Typegeist.Server.Dispatcher
+{
+    internal static class DispatcherDataModel
+    {
+        static DispatcherDataModel()
+        {
+            _data = new TypegeistDataSetContainer();
+        }
+
+        public void AddResult(TypegeistResult result)
+        {
+
+        }
+
+        private static TypegeistDataSetContainer _data;
+    }
+}
